51b41e4f65907197a8c71b6346e76407b2ccc1b0
Quyen Le Sy <danquyen6596@gmail.com>
2018-10-24 20:19:00 +0700

    Add file to program.

---
+++ src/vn/codegym/QuyenLeSy/MyLinkListTest.java
@@ -0,0 +1,15 @@
+package vn.codegym.QuyenLeSy;
+
+public class MyLinkListTest {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkList ll = new MyLinkList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+
+        ll.add(4,9);
+        ll.add(4,9);
+        ll.printList();
+    }
+}
